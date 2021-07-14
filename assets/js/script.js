12526e3e7bda1c9efdebf6c4813c3297c3285714
Ian Cohen <ian_cohen90@live.com>
2021-07-14 18:51:35 -0400

    added questions in js file

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var quizQuestions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Tool Market System", "Hyper Text Markup Language", "Hyper Text Mark Learning", "Hyper Tool Marketing System"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "In what tag does the main content of the webpage belong in?",
+        choices: ["<main>", "<section>", "<article>", "<body>"],
+        answer: "<body>"
+    },
+    {
+        question: "How many different heading tags are there?",
+        choices: ["6", "10", "12", "8"],
+        answer: "6"
+    },
+    {
+        question: "What tag do you use to make an unordered list?",
+        choices: ["<ul>", "<ol>", "<li>", "<uli>"],
+        answer: "<ul>"
+    },
+    {
+        question: "What is the correct syntax to create a hyperlink?",
+        choices: ["<a src=", "<a class=", "<a href=", "<a img="],
+        answer: "<a href="
+    },
+    {
+        question: "Who is making the Web standards?",
+        choices: ["Mozilla", "Microsoft", "Google", "The World Wide Web Consortium"],
+        answer: "The World Wide Web Consortium"
+    }
+];
