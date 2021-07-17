6f5857648245f3a52640a841698dffb70006d313
Ian Cohen <ian_cohen90@live.com>
2021-07-17 14:32:22 -0400

    added javascript to file, need to fix highscore section

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,5 +49,142 @@ var quizQuestions = [
     }
 ];
 
+var startBtn = document.querySelector("#start-btn");
+var quizEl = document.querySelector(".quiz-container");
+var endEl = document.querySelector(".end");
+var scoreEl = document.querySelector(".score");
+var counter = 0;
+var currentscore = 99;
+var highScores = [];
 
+var scoreCounter = function() {
+    scoreEl.textContent ="Time Remaining: 100"
+    
+    var scoreInterval = setInterval(function(){
+        if(currentscore > 0 && counter < quizQuestions.length){
+            scoreEl.textContent = "Time Remaining: " + currentscore;
+            currentscore--;
+        }else {
+            clearInterval(scoreInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
 
+var createQuiz = function() {
+    document.querySelector("#welcome").remove();
+    quizEl.classList.remove("hide");
+    
+    nextQ(counter);
+    scoreCounter();
+}
+
+var nextQ = function(index){
+    var qheader = document.querySelector(".qheader");
+    var questionEl = document.querySelector(".questions");
+    var btna = document.getElementById("btn-a");
+    var btnb = document.getElementById("btn-b");
+    var btnc = document.getElementById("btn-c");
+    var btnd = document.getElementById("btn-d");
+
+    qheader.textContent = "Qestion #" + parseInt(index + 1);
+    questionEl.textContent = quizQuestions[index].question;
+    btna.textContent = quizQuestions[index].a;
+    btnb.textContent = quizQuestions[index].b;
+    btnc.textContent = quizQuestions[index].c;
+    btnd.textContent = quizQuestions[index].d;
+
+    btna.addEventListener("click", checkAnswer);
+    btnb.addEventListener("click", checkAnswer);
+    btnc.addEventListener("click", checkAnswer);
+    btnd.addEventListener("click", checkAnswer);
+}
+
+var checkAnswer = function(event) {
+    var clickedBtn = event.target.getAttribute("value");
+    var feedbackEl = document.querySelector(".feedback");
+    feedbackEl.classList.remove("hide");
+
+    if(clickedBtn === quizQuestions[counter].answer){
+        
+        feedbackEl.textContent = "Correct Answer!";
+    }else{
+        if(currentscore>=5){
+            currentscore-= 5;
+            scoreEl.textContent ="Time Remaining: " + currentscore;
+        }
+    feedbackEl.classList.remove("hide");
+    feedbackEl.textContent = "Incorrect Answer!";
+    }
+    counter++;
+    
+    if(counter < quizQuestions.length){
+        nextQ(counter);
+    }else{
+        feedbackEl.remove();
+        endQuiz();
+    }
+}
+
+var endQuiz = function(){
+    quizEl.remove();
+    scoreEl.remove();
+
+    endEl.innerHTML = "<h2 class='end-title'>That's all she wrote!</h2><p>Your final score is " + currentscore + ". Please enter your name.</p>";
+
+    var scoreForm = document.createElement("form");
+    scoreForm.id = "score-form";
+    endEl.appendChild(scoreForm);
+
+    var name = document.createElement("input");
+    name.className = "name-input";
+    name.setAttribute("name", "player-name");
+    name.setAttribute("placeholder", "Enter Your Name");
+    scoreForm.appendChild(name);
+
+    var namebutton = document.createElement("BUTTON");
+    namebutton.className = "btn";
+    namebutton.id =  "name-btn";
+    namebutton.textContent = "SUBMIT";
+    scoreForm.appendChild(namebutton);
+
+    namebutton.addEventListener("click", saveScore);
+}
+
+var saveScore = function() {
+    event.preventDefault();
+    var playerName = document.querySelector("input[name='player-name']").value;
+
+    if(!playerName){
+        alert("Please enter your name!")
+    }else{
+        var scoreObj = {
+            name: playerName,
+            score: currentscore
+        }
+
+        highScores.push(scoreObj);
+        document.querySelector("#score-form").reset();
+        localStorage.setItem("scores", JSON.stringify(highScores));
+    }
+}
+
+var loadScores = function() { 
+    highScores = localStorage.getItem("score");
+
+    if (!highScores) {
+        highScores = []
+
+        var noScores = document.createElement("div");
+        noScores.setAttribute("style", "text-align: center");
+        noScores.textContent = "There are no scores yet!  Play the quiz to add your score!"
+        document.querySelector("#score-card").appendChild(noScores);
+
+        return false;
+    }
+
+    highScores = JSON.parse(highScores);
+    highScores.sort(compare);
+}
+
+startBtn.addEventListener("click", createQuiz);
